371ef0b62e3432a815f675787b92f9c39b7ae21e
Nelwey <nelwey1234@gmail.com>
2020-03-03 23:49:17 -0400

    quitando la validacion del email

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -24,23 +24,23 @@ indexCtrl.createPost = async (req, res, next) => {
   } = req.body;
   console.log(req.body);
 
-  if(song1 == song2 || song1 == song3 || song1 == song4 || song1 == song5){
+  if (song1 == song2 || song1 == song3 || song1 == song4 || song1 == song5) {
     req.flash('error_msg', 'No se puede enviar dos o más canciones con el mismo nombre');
     return res.redirect('/post');
   }
-  if(song2 == song1 || song2 == song3 || song2 == song4 || song2 == song5){
+  if (song2 == song1 || song2 == song3 || song2 == song4 || song2 == song5) {
     req.flash('error_msg', 'No se puede enviar dos o más canciones con el mismo nombre');
     return res.redirect('/post');
   }
-  if(song3 == song1 || song3 == song2 || song3 == song4 || song3 == song5){
+  if (song3 == song1 || song3 == song2 || song3 == song4 || song3 == song5) {
     req.flash('error_msg', 'No se puede enviar dos o más canciones con el mismo nombre');
     return res.redirect('/post');
   }
-  if(song4 == song1 || song4 == song2 || song4 == song3 || song4 == song5){
+  if (song4 == song1 || song4 == song2 || song4 == song3 || song4 == song5) {
     req.flash('error_msg', 'No se puede enviar dos o más canciones con el mismo nombre');
     return res.redirect('/post');
   }
-  if(song5 == song1 || song5 == song2 || song5 == song4 || song5 == song4){
+  if (song5 == song1 || song5 == song2 || song5 == song4 || song5 == song4) {
     req.flash('error_msg', 'No se puede enviar dos o más canciones con el mismo nombre');
     return res.redirect('/post');
   }
@@ -57,26 +57,18 @@ indexCtrl.createPost = async (req, res, next) => {
     date
   });
 
-  const fbEmail = await Post.findOne({email});
+  const postCreated = await newPost.save();
 
-  if (!fbEmail) {
-    const postCreated = await newPost.save();
-
-    if (postCreated) {
-      console.log('Post creado');
-      req.flash('success_msg', 'Publicacion creada correctamente!');
-      return res.redirect('/posts');
-    } else {
-      console.log('Error al crear la publicación');
-      req.flash('error_msg', 'Error al publicar, verifica las canciones ingresadas por favor!');
-      return res.redirect('/post');
-    }
-
-  }else{
-    req.flash('error_msg', 'Este correo ya realizó una publicación, ve y búscalo en las publicaciones ;)');
-    res.redirect('/');
-    console.log('nel, ya esta ese correo');
+  if (postCreated) {
+    console.log('Post creado');
+    req.flash('success_msg', 'Yei, gracias por publicar!');
+    return res.redirect('/posts');
+  } else {
+    console.log('Error al crear la publicación');
+    req.flash('error_msg', 'Error al publicar, verifica las canciones ingresadas por favor!');
+    return res.redirect('/post');
   }
+
 }
 
 
@@ -85,9 +77,13 @@ indexCtrl.createPost = async (req, res, next) => {
 
 indexCtrl.renderPosts = async (req, res, next) => {
 
-  const posts = await Post.find().sort({createdAt:'desc'});
+  const posts = await Post.find().sort({
+    createdAt: 'desc'
+  });
 
-  res.render('posts', {posts} );
+  res.render('posts', {
+    posts
+  });
 
 }
 
